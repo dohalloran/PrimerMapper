95900b4a0a6c7e9198f7ef2b01ae6bbc4d2c2899
dohalloran <damienmohalloran@yahoo.co.uk>
2015-11-12 18:53:35 -0500

    updated changes

--- PrimerMapper_Browser/primermapper.js
+++ PrimerMapper_Browser/primermapper.js
@@ -20,6 +20,112 @@ window.onload = function () {
 }
 
 
+var nn_s = {
+    'AA': '240',
+    'AC': '173',
+    'AG': '208',
+    'AT': '239',
+    'AN': '215',
+    'CA': '129',
+    'CC': '266',
+    'CG': '278',
+    'CT': '208',
+    'CN': '220',
+    'GA': '135',
+    'GC': '267',
+    'GG': '266',
+    'GT': '173',
+    'GN': '210',
+    'TA': '169',
+    'TC': '135',
+    'TG': '129',
+    'TT': '240',
+    'TN': '168',
+    'NA': '168',
+    'NC': '210',
+    'NG': '220',
+    'NT': '215',
+    'NN': '203',
+    'aa': '240',
+    'ac': '173',
+    'ag': '208',
+    'at': '239',
+    'an': '215',
+    'ca': '129',
+    'cc': '266',
+    'cg': '278',
+    'ct': '208',
+    'cn': '220',
+    'ga': '135',
+    'gc': '267',
+    'gg': '266',
+    'gt': '173',
+    'gn': '210',
+    'ta': '169',
+    'tc': '135',
+    'tg': '129',
+    'tt': '240',
+    'tn': '168',
+    'na': '168',
+    'nc': '210',
+    'ng': '220',
+    'nt': '215',
+    'nn': '203'
+}
+var nn_h = {
+    'AA': '91',
+    'AC': '65',
+    'AG': '78',
+    'AT': '86',
+    'AN': '80',
+    'CA': '58',
+    'CC': '110',
+    'CG': '119',
+    'CT': '78',
+    'CN': '91',
+    'GA': '56',
+    'GC': '111',
+    'GG': '110',
+    'GT': '65',
+    'GN': '85',
+    'TA': '60',
+    'TC': '56',
+    'TG': '58',
+    'TT': '91',
+    'TN': '66',
+    'NA': '66',
+    'NC': '85',
+    'NG': '91',
+    'NT': '80',
+    'NN': '80',
+    'aa': '91',
+    'ac': '65',
+    'ag': '78',
+    'at': '86',
+    'an': '80',
+    'ca': '58',
+    'cc': '110',
+    'cg': '119',
+    'ct': '78',
+    'cn': '91',
+    'ga': '56',
+    'gc': '111',
+    'gg': '110',
+    'gt': '65',
+    'gn': '85',
+    'ta': '60',
+    'tc': '56',
+    'tg': '58',
+    'tt': '91',
+    'tn': '66',
+    'na': '66',
+    'nc': '85',
+    'ng': '91',
+    'nt': '80',
+    'nn': '80'
+}
+
+
 function loaderMy() {
 
     var gene_length = "";
@@ -143,7 +249,12 @@ function loaderMy() {
     //////////////////////////////
     /////DRAW PRIMERS 
 
-    var canvasNewer = new fabric.Canvas('canvas3');
+    var canvasNewer = new fabric.Canvas('canvas3', {
+        hoverCursor: 'pointer',
+        selection: false
+    });
+
+    fabric.Object.prototype.transparentCorners = false;
 
     function drawMyPrimers() {
         canvasNewer.add(new fabric.Text('draw primers:', {
@@ -197,14 +308,21 @@ function loaderMy() {
             var endScaled = Math.round(Math.round(funcAdjust(endPrimer) - 12.5) * 15.9);
             var primer = dnaForPrimers.substring(startScaled, endScaled);
 
+            if (primer.length > 36) {
             if (endScaled >= startScaled) {
-                alert(primer);
+                alert("Forward Primer: " + primer + "\n" + "GC percent: " + gcPercent(primer) + "%" + "\n" + "Primer Tm: " + calcTmLong(primer) + "\u2103");
             } else {
-                alert(reverse(primer));
+                alert("Reverse Primer: " + reverse(primer) + "\n" + "GC percent: " + gcPercent(reverse(primer)) + "%" + "\n" + "Primer Tm: " + calcTmLong(reverse(primer)) + "\u2103");
+            }
+            } else {
+               if (endScaled >= startScaled) {
+                alert("Forward Primer: " + primer + "\n" + "GC percent: " + gcPercent(primer) + "%" + "\n" + "Primer Tm: " + calcTm(primer) + "\u2103");
+            } else {
+                alert("Reverse Primer: " + reverse(primer) + "\n" + "GC percent: " + gcPercent(reverse(primer)) + "%" + "\n" + "Primer Tm: " + calcTm(reverse(primer)) + "\u2103");
+            }     
             }
             getPosit = [];
         });
-
     }
 
     drawMyPrimers();
@@ -361,12 +479,60 @@ function loaderMy() {
             context.font = "8px Arial";
             context.fillText(primer_start_R[j], adjusted_primer_start_R + 62, leftOver_R + 3);
 
-
-
         }
 
     }
 
+    //Calculate Tm of Primer if >36bps
+    function calcTmLong(dna) {
+        var numberTmLong = 81.5 + (16.6 * (Math.log(50 / 1000.0) / Math.log(10))) +
+            (41.0 * (gcPercent(dna) / 100)) - (600.0 / dna.length);
+        return numberTmLong.toFixed(2);
+    }
+
+    //Calculate Tm of Primer if <=36bps
+    function calcTm(sequence) {
+        var dH = 0;
+        var dS = 108;
+        var i;
+        // Compute dH and dS
+        for (i = 0; i < (sequence.length - 1); i++) {
+            var pair = sequence.substr(i, 2);
+            dH += parseInt(nn_h[pair], 10);
+            dS += parseInt(nn_s[pair], 10);
+        }
+        dH *= -100.0;
+        dS *= -0.1;
+        var numberTm = dH / (dS + 1.987 * Math.log(100 / 4000000000.0)) - 273.15 +
+            16.6 * (Math.log(50 / 1000.0) / Math.log(10));
+        return numberTm.toFixed(2);
+    }
+
+
+    //Calculate GC Percentage of Primer
+    function gcPercent(dna) {
+        var Arr_Primer = dna.split("");
+        var gCount = 0;
+        var tCount = 0;
+        var cCount = 0;
+        var aCount = 0;
+        for (var i = 0; i < Arr_Primer.length; i++) {
+            if (Arr_Primer[i] === 'A') {
+                aCount++;
+            } else if (Arr_Primer[i] === 'C') {
+                cCount++;
+            } else if (Arr_Primer[i] === 'T') {
+                tCount++;
+            } else if (Arr_Primer[i] === 'G') {
+                gCount++;
+            }
+        }
+        var number = ((gCount + cCount) / Arr_Primer.length) * 100;
+        return number.toFixed(2);
+    }
+
+
+    //Reverse Complement Primer
     function reverse(s) {
         return s.split("").map(complement).reverse().join("");
     }
@@ -385,7 +551,6 @@ function loaderMy() {
 
 
     function funcAdjust(start_bp) {
-
         var adjusted = (start_bp / 666) * 167;
         return adjusted;
     }
